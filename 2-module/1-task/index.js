db09d20c26f7c53b3df399af637c28e1fa8f68d6
virus <crimeacriminal@gmail.com>
2025-04-07 16:10:32 +0300

    done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let isHasSalary = false;
+  for (const key in salaries) {
+    if (typeof salaries[key] === 'number' && Number.isFinite(salaries[key]))
+    {
+      isHasSalary = true;
+      sum += salaries[key];
+    }
+    
+  }
+  if (isHasSalary) {
+    return sum;
+  } else {
+    return 0;
+  }
+
+  
 }
+
