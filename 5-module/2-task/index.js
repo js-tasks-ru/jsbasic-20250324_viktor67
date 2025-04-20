ca001be4bf10918eb38fca3cab22bb0c7d9f498b
virus <crimeacriminal@gmail.com>
2025-04-20 09:25:08 +0300

    done homework module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  const text = document.querySelector('#text');
+  const button = document.querySelector('.toggle-text-button');
+
+  button.addEventListener('click', ()=>{
+    if (text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,54 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const innerCarousel = document.querySelector('.carousel__inner');
+  const slides = innerCarousel.querySelectorAll('.carousel__slide');
+
+
+  let currentSlide = 0;
+  const totalSlides = slides.length;
+  let widthElem = innerCarousel.offsetWidth;
+  arrowLeft.style.display = 'none';
+  
+  arrowRight.addEventListener('click', ()=>{
+    if (currentSlide <= totalSlides) {
+      currentSlide++;
+      const offset = -widthElem * currentSlide;
+      innerCarousel.style.transform = `translateX(${offset}px)`;
+
+      if (currentSlide === 0) {
+        arrowLeft.style.display = 'none';
+      } else {
+        arrowLeft.style.display = '';
+      }
+
+      if (currentSlide === totalSlides - 1) {
+        arrowRight.style.display = 'none';
+      } else {
+        arrowRight.style.display = '';
+      }
+    }
+  });
+
+  arrowLeft.addEventListener('click', ()=>{
+    if (currentSlide > 0) {
+      currentSlide--;
+      const offset = -widthElem * currentSlide;
+      innerCarousel.style.transform = `translateX(${offset}px)`;
+
+      if (currentSlide === 0) {
+        arrowLeft.style.display = 'none';
+      } else {
+        arrowLeft.style.display = '';
+      }
+
+      if (currentSlide === totalSlides - 1) {
+        arrowRight.style.display = 'none';
+      } else {
+        arrowRight.style.display = '';
+      }
+    }
+  });
+
+  
 }
