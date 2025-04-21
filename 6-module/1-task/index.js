86facaba8145fcf5ca04fcb535c11f8cec4292da
virus <crimeacriminal@gmail.com>
2025-04-21 07:21:12 +0300

    done homework 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
+  }
+
+  createTable() {
+    const table = document.createElement('table');
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+
+    const tbody = table.querySelector('tbody');
+
+    for (const row of this.rows) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+
+      // Добавляем обработчик на кнопку удаления
+      tr.querySelector('button').addEventListener('click', () => {
+        tr.remove();
+      });
+
+      tbody.appendChild(tr);
+    }
+
+    return table;
   }
 }
